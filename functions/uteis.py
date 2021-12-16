bfa29b6cd5ff61a7d2c0ca692cdbb0e0080d7b45
Pedro Ferreira <64147890+pedrovictor48@users.noreply.github.com>
2021-12-15 21:24:47 -0300

    Create uteis.py

---
+++ functions/uteis.py
@@ -0,0 +1,9 @@
+def toTable(texto: str) -> list:
+    converter = lambda x: ord(x) - ord('a') + 2 if x != ' ' else 28
+    vetor = list(map(converter, texto))
+    return vetor
+
+def toText(vetor: list) -> str:
+    converter = lambda x: chr(x - 2 + ord('a')) if x != 28 else ' '
+    s = ''.join(list(map(converter, vetor)))
+    return s
