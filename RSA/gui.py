46c2ec8529d3b6c8d7605fe545fe143c221472e9
Vinícius Teixeira <92376175+VinnieT1@users.noreply.github.com>
2021-12-22 16:53:15 -0300

    Update gui.py

--- RSA/gui.py
+++ RSA/gui.py
@@ -25,7 +25,7 @@ class ChavePub:
                 elif f.eh_primo(int(self.valores['p'])) and f.eh_primo(int(self.valores['q'])):
                     n = int(self.valores['p'])*int(self.valores['q'])
                     e = int(self.valores['e'])
-                    with open('chave_publica.txt', 'w') as file:
+                    with open('chaves_publicas.txt', 'w') as file:
                         file.write(str(n) + '\n')
                         file.write(str(e) + '\n')
                     sg.popup('Chave gerada com sucesso!')
@@ -33,6 +33,36 @@ class ChavePub:
                 else:
                     sg.popup_error('Número(s) inválido(s)!')
                     print('Numeros invalidos')
+
+class Encript:
+    def __init__(self):
+        layout = [
+            [sg.Text('Insira a mensagem:')],
+            [sg.Input(key = 'msg')],
+            [sg.Button('Encriptar'), sg.Button('Sair')]
+        ]
+        sg.theme('DarkTeal7')
+        self.janela = sg.Window('Encriptar', layout)
+
+    def Run(self):
+        while True:
+            self.eventos, self.valores = self.janela.read()
+            if self.eventos == sg.WINDOW_CLOSED or self.eventos == 'Sair':
+                self.janela.close()
+                break
+            if self.eventos == 'Encriptar':
+                try:
+                    with open('chaves_publicas.txt', 'r') as file:
+                        n = int(file.readline())
+                        e = int(file.readline())
+                        print(n)
+                        print(e)
+                        #CONTINUACAO...
+                except:
+                    sg.popup('Você ainda não definiu as chaves públicas!')
+                    print('Chaves inexistentes')
+                    self.janela.close()
+                    break
 
 class Hub:
     def __init__(self):
@@ -44,14 +74,17 @@ class Hub:
             [sg.Button('Sair')]
         ]
         sg.theme('DarkTeal7')
-        self.hub = sg.Window('Criptografia RSA', layout, element_justification = 'c', size = (300, 160))
+        self.janela = sg.Window('Criptografia RSA', layout, element_justification = 'c', size = (300, 160))
 
     def Run(self):
         while True:
-            self.eventos, self.valores = self.hub.read()
+            self.eventos, self.valores = self.janela.read()
             if self.eventos == sg.WINDOW_CLOSED or self.eventos == 'Sair':
-                self.hub.close()
+                self.janela.close()
                 break
             if self.eventos == 'Gerar Chave Pública':
                 geracao_chave = ChavePub()
                 geracao_chave.Run()
+            if self.eventos == 'Encriptar':
+                encriptar = Encript()
+                encriptar.Run()
